ae74e5abb880cf3867ec4d1198949802e3391b85
Aakarsh <sngh15721@gmail.com>
2025-12-19 15:29:19 +1030

    Finished greedy notes and implementation

---
+++ Leetcode_DSA_course/8 - Greedy/asteroids_destroyed.py
@@ -0,0 +1,11 @@
+class Solution:
+    def asteroids_destroyed(self, mass: int, asteroids: list[int]) -> bool:
+        asteroids.sort()
+
+        for asteroid in asteroids:
+            if (asteroid > mass):
+                return False
+            
+            mass += asteroid
+
+        return True
