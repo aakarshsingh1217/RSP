adf0e3553341195c3e87550f8797c6c203a87564
Aakarsh <sngh15721@gmail.com>
2025-12-23 16:17:51 +1030

    Finished DP section in Leetcode DSA course, notes and implementation

---
+++ Leetcode_DSA_course/11 - Dynamic programming/fib_memoize.py
@@ -0,0 +1,14 @@
+def fibonacci(n):
+    if (n == 0):
+        return 0
+    if (n == 1):
+        return 1
+    
+    if (n in memo):
+        return memo[n]
+    
+    memo[n] = fibonacci(n - 1) + fibonacci(n - 2)
+
+    return memo[n]
+
+memo = {}
